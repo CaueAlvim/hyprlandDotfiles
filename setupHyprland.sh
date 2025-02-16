8417776d441c0f2e116363ef1d30b466ad5b860b
CaueMiyazaki <83715093+CaueAlvim@users.noreply.github.com>
2025-02-16 02:19:23 +0000

    Update setupHyprland.sh

--- setupHyprland.sh
+++ setupHyprland.sh
@@ -10,7 +10,7 @@ sudo pacman -Syu --noconfirm
 sleep 0.5
 
 # Install essential packages
-sudo pacman -S hyprland hypridle hyprlock kitty waybar swaync xdg-desktop-portal-hyprland xdg-user-dirs ufw fwupd nano grim slurp swww ttf-dejavu ttf-dejavu-nerd cantarell-fonts --noconfirm
+sudo pacman -S hyprland hypridle hyprlock kitty waybar swaync xdg-desktop-portal-hyprland xdg-user-dirs ufw fwupd sbctl nano grim slurp swww ttf-dejavu ttf-dejavu-nerd cantarell-fonts --noconfirm
 sleep 0.5
 
 # Install some other stuff
@@ -21,19 +21,7 @@ sleep 0.5
 sudo pacman -S firefox swayimg gvfs-mtp btop thunar thunar-volman tumbler --noconfirm
 sleep 0.5
 
-# Create default folders
-if [[ -d "$HOME/.config" ]]; then
-   echo "config folder already exists"
-else
-   echo "creating config folder"
-   mkdir $HOME/.config
-fi
-
-cp -r $HOME/hyprlandDotfiles/* $HOME/.config/
-
-
 clear
-sleep 0.5
 echo "receba"
 echo "ATIVAR ufw E greetd, substitua o comando default no arquivo /etc/greetd/config.toml"
 echo "Atualizar firmware e rodar xdg-user-dirs-update"
